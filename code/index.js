faf0081faaa152d760f0eba3327e758b5d5169a8
yzz <1277745281@qq.com>
2018-01-26 19:16:34 +0800

    -

--- code/index.js
+++ code/index.js
@@ -1,6 +1,7 @@
 var arr = [
     <h1>菜鸟教程</h1>,
     <h2>学的不仅是技术，更是梦想！</h2>
-  ];
+];
 ReactDOM.render(
-    <div>{arr}</div>, document.getElementById('example'))
+    <div>{arr}</div>,
+    document.getElementById('example'))
