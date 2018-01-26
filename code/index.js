5bce6f1971eb8b119b59dd67e571d4a904ab8eb0
yzz <1277745281@qq.com>
2018-01-26 11:13:44 +0800

    -

---
+++ code/index.js
@@ -0,0 +1,9 @@
+var myStyle = {
+    fontSize: 100,
+    color: '#FF0000'
+};
+ReactDOM.render(
+    <div>
+    <h1 style={myStyle}>cer</h1>
+    {/*注释*/}
+</div>, document.getElementById('example'))
