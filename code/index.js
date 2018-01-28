ab9f0971ae54275d5ecb51f2ffb9d057f9b00b01
yzz <1277745281@qq.com>
2018-01-28 15:04:25 +0800

    -

--- code/index.js
+++ code/index.js
@@ -1,7 +1,31 @@
-var arr = [
-    <h1>菜鸟教程</h1>,
-    <h2>学的不仅是技术，更是梦想！</h2>
-];
+//复合组件
+var Name = React.createClass({
+    render: function () {
+        return (
+            <h1>{this.props.name}</h1>
+        );
+    }
+})
+var Link = React.createClass({
+    render: function () {
+        return (
+            <a href={this.props.site}>
+                {this.props.site}
+            </a>
+        );
+    }
+})
+var WebSite = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <Name name={this.props.name} />
+                <Link site={this.props.site} />
+            </div>
+        );
+    }
+})
 ReactDOM.render(
-    <div>{arr}</div>,
-    document.getElementById('example'))
+    <WebSite name="菜鸟教程" site="http://www.baidu.com" ></WebSite>,
+    document.getElementById("example")
+)
